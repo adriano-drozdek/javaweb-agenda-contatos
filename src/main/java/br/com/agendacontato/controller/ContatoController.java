665188cee023439f2b2b14572f69f2e66844b619
Adriano Drozdek <61703@PROWAY.TREINA>
2021-04-24 18:17:12 -0300

    metodo novo contato controller

--- src/main/java/br/com/agendacontato/controller/ContatoController.java
+++ src/main/java/br/com/agendacontato/controller/ContatoController.java
@@ -1,31 +1,39 @@
 package br.com.agendacontato.controller;
 
-import java.io.IOException;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = { "/ContatoControler", "/main" })
+@WebServlet(urlPatterns = {"/ContatoController", "/main", "/insert"})
 public class ContatoController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+;
 
-	private static final long serialVersionUID = 1L;
+    public ContatoController() {
+        super();
+    }
 
-	public ContatoController() {
-		super();
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getServletPath();
+        if (action.equals("/main")) {
+            listaContatos(request, response);
+        } else if (action.equals("/insert")) {
+            novoContatos(request, response);
+        }
+    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String action = request.getServletPath();
-		if (action.equals("/main")) {
-			listaContatos(request, response);
-		}
-	}
+    protected void listaContatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("agenda.jsp");
+    }
 
-	protected void listaContatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 response.sendRedirect("agenda.jsp");
-	 }
+    protected void novoContatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(request.getParameter("nome"));
+        System.out.println(request.getParameter("fone"));
+        System.out.println(request.getParameter("email"));
+    }
 }
+
