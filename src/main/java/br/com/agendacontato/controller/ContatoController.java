d28891df4ba0a6169e3d1d6e069e05ff3afd74db
Adriano Drozdek <61703@PROWAY.TREINA>
2021-05-01 17:36:27 -0300

    lincando formulario com backend

--- src/main/java/br/com/agendacontato/controller/ContatoController.java
+++ src/main/java/br/com/agendacontato/controller/ContatoController.java
@@ -1,15 +1,22 @@
 package br.com.agendacontato.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.agendacontato.model.dao.ContatoDao;
+import br.com.agendacontato.model.entity.Contato;
+
 @WebServlet(urlPatterns = { "/ContatoController", "/main", "/insert" })
 public class ContatoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;;
+	private Contato contato;
+	private ContatoDao contatoDao;
 
 	public ContatoController() {
 		super();
@@ -30,10 +37,17 @@ public class ContatoController extends HttpServlet {
 		response.sendRedirect("agenda.jsp");
 	}
 
-	protected void novoContatos(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		System.out.println(request.getParameter("nome"));
-		System.out.println(request.getParameter("fone"));
-		System.out.println(request.getParameter("email"));
+	protected void novoContatos(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		contato = new Contato(request.getParameter("nome"), request.getParameter("fone"),
+				request.getParameter("email"));
+		contato.setObservacao(request.getParameter("observacao"));
+		try {
+			contatoDao = new ContatoDao();
+			contatoDao.create(contato);
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		response.sendRedirect("main");
 	}
 }
