9b57433584fa2fc642a7ebaafc9483bb5f771258
Adriano Drozdek <61703@PROWAY.TREINA>
2021-05-01 15:59:16 -0300

    dao generico

---
+++ src/main/java/br/com/agendacontato/model/dao/BaseDao.java
@@ -0,0 +1,21 @@
+package br.com.agendacontato.model.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Definir as assinaturas dos metodos de CRUD
+ */
+public interface BaseDao<T, ID> {
+
+	void create(T entity) throws SQLException;
+
+	List<T> findAll() throws SQLException;
+
+	T findById(ID id);
+
+	void update(T entity);
+
+	void delete(ID id);
+
+}
