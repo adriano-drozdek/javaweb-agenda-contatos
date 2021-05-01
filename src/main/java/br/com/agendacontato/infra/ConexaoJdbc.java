9acf44421fbbd92a825e25c2db60b0c8899d7a84
Adriano Drozdek <61703@PROWAY.TREINA>
2021-05-01 15:39:45 -0300

    classe de acesso ao mysql

---
+++ src/main/java/br/com/agendacontato/infra/ConexaoJdbc.java
@@ -0,0 +1,14 @@
+package br.com.agendacontato.infra;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConexaoJdbc {
+	Connection getConnection();
+
+	void close();
+
+	void commit() throws SQLException;
+
+	void rollback();
+}
